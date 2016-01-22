19de7dfaddaa0c4fe3811db609c241780280e7ef
Joshua Haertel <josh.haertel@gmail.com>
2016-01-22 07:04:18 -0700

    modelfaced, inspector, corresponding exceptions and needed resource from original code

---
+++ src/shared/models/exceptions/PlayingException.java
@@ -0,0 +1,9 @@
+package shared.models.exceptions;
+
+/** This class is the exception when a function that is called during the
+ * move state.
+ */
+@SuppressWarnings("serial")
+public class PlayingException extends Exception {
+
+}
