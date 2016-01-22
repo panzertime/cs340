44c3ae7d49919a2bc85b4a5d8fc424b0dd75a025
Jacob Barzee <jbarzee@qualtrics.com>
2016-01-22 01:02:11 -0700

    Fixed Misspelling of Piece Package

---
+++ src/shared/models/board/piece/Road.java
@@ -0,0 +1,37 @@
+package shared.models.board.piece;
+
+import shared.models.Player;
+import shared.models.board.edge.Edge;
+
+public class Road {
+
+	protected Player owner;
+	protected Edge edge;
+	
+	public Road (Player owner) throws NullPlayerException {
+		if (owner == null)
+			throw new NullPlayerException();
+		this.owner = owner;
+	}
+	
+	/**
+	 * @return the edge
+	 */
+	public Edge getEdge() {
+		return edge;
+	}
+	
+	/**
+	 * @param edge the edge to set
+	 */
+	public void setEdge(Edge edge) {
+		this.edge = edge;
+	}
+	
+	/**
+	 * @return the owner
+	 */
+	public Player getOwner() {
+		return owner;
+	}
+}
