74e598d493997bc48fa922a1c9f012ce28d83d37
Joshua Haertel <josh.haertel@gmail.com>
2016-03-11 23:01:37 -0700

    Added mock command stubs

---
+++ java/src/server/command/mock/games/create.java
@@ -0,0 +1,16 @@
+package server.command.mock.games;
+
+import org.json.simple.JSONObject;
+
+import server.exception.ServerAccessException;
+
+public class create extends GamesCommand {
+
+	@Override
+	public String execute(JSONObject args, String cookie) throws ServerAccessException {
+		return null;
+		// TODO Auto-generated method stub
+
+	}
+
+}
