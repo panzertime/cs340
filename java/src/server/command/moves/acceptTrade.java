923b5e538d4cb138e485fff98ff81f2e7f96e39a
Joshua Haertel <josh.haertel@gmail.com>
2016-03-23 11:54:50 -0600

    acceptTrade Moves command

--- java/src/server/command/moves/acceptTrade.java
+++ java/src/server/command/moves/acceptTrade.java
@@ -2,15 +2,40 @@ package server.command.moves;
 
 import org.json.simple.JSONObject;
 
+import server.data.User;
 import server.exception.ServerAccessException;
+import shared.model.Model;
+import shared.model.exceptions.ViolatedPreconditionException;
 
 public class acceptTrade extends MovesCommand {
 
 	@Override
-	public String execute(JSONObject args, String cookie) throws ServerAccessException {
-		return null;
-		// TODO Auto-generated method stub
-
+	public String execute(JSONObject args, String cookie)
+			throws ServerAccessException {
+		String result = null;
+		if(validCookie(cookie)) {
+			if(validMovesArguments(args, getClass().getSimpleName())) {
+				try {
+					boolean willAccept = (Boolean) args.get("willAccept");
+					Model game = getGameFromCookie(cookie);
+					User user = getUserFromCookie(cookie);
+					int playerIndex = 
+							((Long) args.get("playerIndex")).intValue();
+					try {
+						game.doAcceptTrade(willAccept, playerIndex);
+					} catch (Exception e) {
+						
+					}
+				} catch (Exception e) {
+					throw new ServerAccessException("Invalid Parameters: "
+							+ "willAccept");
+				}
+			} else {
+				throw new ServerAccessException("Invalid Parameters");
+			}
+		} else {
+			throw new ServerAccessException("Invalid Cookie");
+		}
+		return result;
 	}
-
 }
