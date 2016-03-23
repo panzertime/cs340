8fa3fe36b2b43d00da5a530518b5750d700c1c1a
Joshua Haertel <josh.haertel@gmail.com>
2016-03-23 16:24:13 -0600

    sendChat, Soldier

--- java/src/server/command/moves/Soldier.java
+++ java/src/server/command/moves/Soldier.java
@@ -3,14 +3,42 @@ package server.command.moves;
 import org.json.simple.JSONObject;
 
 import server.exception.ServerAccessException;
+import shared.model.Model;
+import shared.model.board.hex.HexLocation;
+import shared.model.exceptions.ViolatedPreconditionException;
 
 public class Soldier extends MovesCommand {
 
 	@Override
-	public String execute(JSONObject args, String cookie) throws ServerAccessException {
-		return null;
-		// TODO Auto-generated method stub
-
+	public String execute(JSONObject args, String cookie) 
+			throws ServerAccessException {
+		String result = null;
+		if(validCookie(cookie)) {
+			if(validMovesArguments(args, getClass().getSimpleName())) {
+				Model game = getGameFromCookie(cookie);
+				int playerIndex = 
+						((Long) args.get("playerIndex")).intValue();
+				HexLocation robLocation = 
+						makeHexLocation(args.get("location"));
+				try {
+					int victimIndex = 
+							((Long) args.get("victimIndex")).intValue();
+					game.doSoldier(robLocation, victimIndex, playerIndex);
+					JSONObject resultJSON = game.toJSON();
+					result = resultJSON.toJSONString();
+				} catch (ViolatedPreconditionException e) {
+					throw new ServerAccessException("Unable to "
+							+ "perform move");
+				} catch (Exception e) {
+					throw new ServerAccessException("Invalid Parameter: "
+							+ "victimIndex");
+				}
+			} else {
+				throw new ServerAccessException("Invalid Parameters");
+			}
+		} else {
+			throw new ServerAccessException("Invalid Cookie");
+		}
+		return result;		
 	}
-
 }
--- java/src/server/command/moves/sendChat.java
+++ java/src/server/command/moves/sendChat.java
@@ -3,14 +3,38 @@ package server.command.moves;
 import org.json.simple.JSONObject;
 
 import server.exception.ServerAccessException;
+import shared.model.Model;
+import shared.model.exceptions.ViolatedPreconditionException;
 
 public class sendChat extends MovesCommand {
 
 	@Override
-	public String execute(JSONObject args, String cookie) throws ServerAccessException {
-		return null;
-		// TODO Auto-generated method stub
-		
+	public String execute(JSONObject args, String cookie)
+			throws ServerAccessException {
+		String result = null;
+		if(validCookie(cookie)) {
+			if(validMovesArguments(args, getClass().getSimpleName())) {
+				Model game = getGameFromCookie(cookie);
+				int playerIndex = 
+						((Long) args.get("playerIndex")).intValue();
+				try {
+					String content = (String) args.get("content");
+					game.doSendChat(content, playerIndex);
+					JSONObject resultJSON = game.toJSON();
+					result = resultJSON.toJSONString();
+				} catch (ViolatedPreconditionException e) {
+					throw new ServerAccessException("Unable to "
+							+ "perform move");
+				} catch (Exception e) {
+					throw new ServerAccessException("Invalid Parameter: "
+							+ "content");
+				}
+			} else {
+				throw new ServerAccessException("Invalid Parameters");
+			}
+		} else {
+			throw new ServerAccessException("Invalid Cookie");
+		}
+		return result;		
 	}
-
 }
