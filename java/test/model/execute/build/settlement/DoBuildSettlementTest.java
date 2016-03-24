ecccc8c50be1cad386744d25f83284fd7caaecc8
Jacob Barzee <jbarzee@qualtrics.com>
2016-03-23 19:18:31 -0600

    doBuildSettlemen tests

--- java/test/model/execute/build/settlement/DoBuildSettlementTest.java
+++ java/test/model/execute/build/settlement/DoBuildSettlementTest.java
@@ -11,32 +11,20 @@ import java.util.Scanner;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
-import org.junit.Before;
 import org.junit.Test;
 
-import client.main.ClientPlayer;
-import client.modelfacade.CanModelFacade;
-import client.modelfacade.ModelFacade;
-import client.modelfacade.testing.TestingModelFacade;
+import shared.model.Model;
 import shared.model.board.hex.HexLocation;
 import shared.model.board.vertex.VertexDirection;
 import shared.model.board.vertex.VertexLocation;
 import shared.model.exceptions.BadJSONException;
+import shared.model.exceptions.ViolatedPreconditionException;
 
 public class DoBuildSettlementTest {
 	
-	@Before
-	public void initFacades() {
-		//CanModelFacade.sole().setUserIndex(0);
-		//TestingModelFacade.sole().setUserIndex(0);
-		ClientPlayer.sole().setUserIndex(0);
-		TestingModelFacade.sole().emptyModel();
-	}
-
-
-	public void initModel(String file) {
+	public JSONObject getJSONFrom(String file) {
 		JSONParser parser = new JSONParser();
-		File jsonFile = new File("java/bin/test/model/can/build/settlement/" + file);
+		File jsonFile = new File("java/bin/test/model/execute/build/settlement/" + file);
 		FileInputStream fis;
 		try {
 			fis = new FileInputStream(jsonFile);
@@ -48,166 +36,222 @@ public class DoBuildSettlementTest {
 				x += "\n";
 			}
 			scanner.close();
+			return (JSONObject) parser.parse(x);
 			
-			JSONObject jsonModel = (JSONObject) parser.parse(x);
-			ModelFacade.setModel(jsonModel);
-			
-		} catch (FileNotFoundException | ParseException | BadJSONException e) {
+		} catch (FileNotFoundException | ParseException e) {
 			e.printStackTrace();
 		}
+		return null;
 	}
 	
-	/*
-	 * initModel()
-	 * turnIndex = 0
-	 * turn status = Playing
-	 * Road added to (2,0,NW) in JSON
-	 * User has 1 wood, 1 brick, 1 wheat, 1 sheep and 1 settlement
-	 * Try to add (2,0, NW) here
-	 */
 	@Test
-	public void testCanBuildSettlement1() {
-		VertexLocation vertLoc = new VertexLocation(new HexLocation(2,0), VertexDirection.NorthWest);
-		initModel("good.txt");
-		try {
-			if(CanModelFacade.sole().canBuildSettlement(vertLoc) == true) {
-				System.out.println("passed testCanBuildSettlement test when in playing mode and test meets parameters");
-			} else {
-				fail("failed testCanBuildSettlement test when in playing mode and test meets parameters");
-			}
-		} catch (NullPointerException e) {
-			fail("failed testCanBuildSettlement test when in playing mode and test meets parameters - model not created");
-		}
-	}
-	
-	/*
-	 * initModel()
-	 * turnIndex = 0
-	 * turn status = SecondRound
-	 * Road taken away from (0,1,SE) in JSON
-	 * User has 0 res,but 4 settlements
-	 * Try to add (0,1,SE) here
-	 */
-	@Test
-	public void testCanBuildSettlement2() {
-		VertexLocation vertLoc = new VertexLocation(new HexLocation(0,1), VertexDirection.SouthEast);
-		initModel("goodSetup.txt");
-		try {
-			if(CanModelFacade.sole().canBuildSettlement(vertLoc) == true) {
-				System.out.println("passed testCanBuildSettlement test when in setup mode and test meets parameters");
-			} else {
-				fail("failed testCanBuildSettlement test when in setup mode and test meets parameters");
-			}
-		} catch (NullPointerException e) {
-			fail("failed testCanBuildSettlement test when in setup mode and test meets parameters - model not created");
-		}
-	}
-	
-	//No model
-	@Test
-	public void testCanBuildSettlement3() {
-		VertexLocation vertLoc = new VertexLocation(new HexLocation(2,0), VertexDirection.NorthWest);
-		try {
-			CanModelFacade.sole().canBuildSettlement(vertLoc);
-			fail("failed testCanBuildSettlement test when no model");
-		} catch (NullPointerException e) {
-			System.out.println("passed testCanBuildSettlement test when no model");
-		}
-	}
-	
-	//Not your turn
-	@Test
-	public void testCanBuildSettlement4() {
-		VertexLocation vertLoc = new VertexLocation(new HexLocation(2,0), VertexDirection.NorthWest);
-		initModel("noTurn.txt");
-		try {
-			if(CanModelFacade.sole().canBuildSettlement(vertLoc) == false) {
-				System.out.println("passed testCanBuildSettlement test when not your turn");
-			} else {
-				fail("failed testCanBuildSettlement test when not your turn");
-			}
-		} catch (NullPointerException e) {
-			fail("failed testCanBuildSettlement test when not your turn - model not created");
-		}
-	}
-	
-	//not playing
-	@Test
-	public void testCanBuildSettlement5() {
-		VertexLocation vertLoc = new VertexLocation(new HexLocation(2,0), VertexDirection.NorthWest);
-		initModel("noPlaying.txt");
-		try {
-			if(CanModelFacade.sole().canBuildSettlement(vertLoc) == false) {
-				System.out.println("passed testCanBuildSettlement test when not in playing mode");
-			} else {
-				fail("failed testCanBuildSettlement test when not in playing mode");
-			}
-		} catch (NullPointerException e) {
-			fail("failed testCanBuildSettlement test when not in playing mode - model not created");
-		}
-	}
-	
-	//not open
-	@Test
-	public void testCanBuildSettlement6() {
+	public void testDoBuildSettlement1() {
 		VertexLocation vertLoc = new VertexLocation(new HexLocation(0,0), VertexDirection.SouthWest);
-		initModel("good.txt");
+		Model model = null;
 		try {
-			if(CanModelFacade.sole().canBuildSettlement(vertLoc) == false) {
-				System.out.println("passed testCanBuildSettlement test when not an open location");
-			} else {
-				fail("failed testCanBuildSettlement test when not an open location");
-			}
-		} catch (NullPointerException e) {
-			fail("failed testCanBuildSettlement test when not an open location - model not created");
+			model = new Model(getJSONFrom("setup-firstSettlement.json"));
+			model.doBuildSettlement(true, vertLoc, 0);
+		} catch (BadJSONException e) {
+			fail("Failed doBuildSettlement while initilizing model");
+		} catch (ViolatedPreconditionException e) {
+			fail("Failed doBuildSettlement while in first setup");
 		}
-	}
-
-	//on water
-	@Test
-	public void testCanBuildSettlement7() {
-		VertexLocation vertLoc = new VertexLocation(new HexLocation(0,3), VertexDirection.SouthWest);
-		initModel("good.txt");
-		try {
-			if(CanModelFacade.sole().canBuildSettlement(vertLoc) == false) {
-				System.out.println("passed testCanBuildSettlement test when placing on water");
-			} else {
-				fail("failed testCanBuildSettlement test when placing on water");
-			}
-		} catch (NullPointerException e) {
-			fail("failed testCanBuildSettlement test when placing on water - model not created");
+		if(model.equalsJSON(getJSONFrom("placed-firstSettlemnet.json"))) {
+			System.out.println("passed doBuildSettlement while in first setup");
+		} else {
+			fail("Failed doBuildSettlement while in first setup");
 		}
 	}
 	
-	//adjacent hex
-		@Test
-		public void testCanBuildSettlement8() {
-			VertexLocation vertLoc = new VertexLocation(new HexLocation(1,1), VertexDirection.NorthWest);
-			initModel("good.txt");
-			try {
-				if(CanModelFacade.sole().canBuildSettlement(vertLoc) == false) {
-					System.out.println("passed testCanBuildSettlement test when placing on an adjacent vertex");
-				} else {
-					fail("failed testCanBuildSettlement test when placing on an adjacent vertex");
-				}
-			} catch (NullPointerException e) {
-				fail("failed testCanBuildSettlement test when placing on an adjacent vertex - model not created");
-			}
-		}
-	
-	//not enough resources
 	@Test
-	public void testCanBuildSettlement9() {
-		VertexLocation vertLoc = new VertexLocation(new HexLocation(2,0), VertexDirection.NorthWest);
-		initModel("noRes.txt");
+	public void testDoBuildSettlement2() {
+		VertexLocation vertLoc = new VertexLocation(new HexLocation(0,1), VertexDirection.SouthWest);
+		Model model = null;
 		try {
-			if(CanModelFacade.sole().canBuildSettlement(vertLoc) == false) {
-				System.out.println("passed testCanBuildSettlement test when not enough resources");
-			} else {
-				fail("failed testCanBuildSettlement test when not enough resources");
-			}
-		} catch (NullPointerException e) {
-			fail("failed testCanBuildSettlement test when not enough resources - model not created");
+			model = new Model(getJSONFrom("setup-secondSettlement.json"));
+			model.doBuildSettlement(true, vertLoc, 0);
+		} catch (BadJSONException e) {
+			fail("Failed doBuildSettlement while initilizing model");
+		} catch (ViolatedPreconditionException e) {
+			fail("Failed doBuildSettlement while in second setup");
+		}
+		if(model.equalsJSON(getJSONFrom("placed-secondSettlemnet.json"))) {
+			System.out.println("passed doBuildSettlement while in second setup");
+		} else {
+			fail("Failed doBuildSettlement while in second setup");
 		}
 	}
+	
+	@Test
+	public void testDoBuildSettlement3() {
+		VertexLocation vertLoc = new VertexLocation(new HexLocation(0,1), VertexDirection.West);
+		Model model = null;
+		try {
+			model = new Model(getJSONFrom("setup-secondSettlement.json"));
+			model.doBuildSettlement(true, vertLoc, 0);
+		} catch (BadJSONException e) {
+			fail("Failed doBuildSettlement while initilizing model");
+		} catch (ViolatedPreconditionException e) {
+			System.out.println("passed doBuildSettlement while in second setup and too close");
+		}
+		if(!model.equalsJSON(getJSONFrom("setup-secondSettlement.json"))) {
+			fail("Failed doBuildSettlement while in second setup and too close");
+		}
+	}
+	
+	@Test
+	public void testDoBuildSettlement4() {
+		VertexLocation vertLoc = new VertexLocation(new HexLocation(0,3), VertexDirection.East);
+		Model model = null;
+		try {
+			model = new Model(getJSONFrom("setup-secondSettlement.json"));
+			model.doBuildSettlement(true, vertLoc, 0);
+		} catch (BadJSONException e) {
+			fail("Failed doBuildSettlement while initilizing model");
+		} catch (ViolatedPreconditionException e) {
+			System.out.println("passed doBuildSettlement while in second setup and on water");
+		}
+		if(!model.equalsJSON(getJSONFrom("setup-secondSettlement.json"))) {
+			fail("Failed doBuildSettlement while in second setup and on water");
+		}
+	}
+	
+	@Test
+	public void testDoBuildSettlement5() {
+		VertexLocation vertLoc = new VertexLocation(new HexLocation(0,1), VertexDirection.West);
+		Model model = null;
+		try {
+			model = new Model(getJSONFrom("playing.json"));
+			model.doBuildSettlement(false, vertLoc, 0);
+		} catch (BadJSONException e) {
+			fail("Failed doBuildSettlement while initilizing model");
+		} catch (ViolatedPreconditionException e) {
+			System.out.println("passed doBuildSettlement while playing and too close");
+		}
+		if(!model.equalsJSON(getJSONFrom("playing.json"))) {
+			fail("Failed doBuildSettlement while playing and too close");
+		}
+	}
+	
+	@Test
+	public void testDoBuildSettlement6() {
+		VertexLocation vertLoc = new VertexLocation(new HexLocation(0,3), VertexDirection.East);
+		Model model = null;
+		try {
+			model = new Model(getJSONFrom("playing.json"));
+			model.doBuildSettlement(false, vertLoc, 0);
+		} catch (BadJSONException e) {
+			fail("Failed doBuildSettlement while initilizing model");
+		} catch (ViolatedPreconditionException e) {
+			System.out.println("passed doBuildSettlement while playing and on water");
+		}
+		if(!model.equalsJSON(getJSONFrom("playing.json"))) {
+			fail("Failed doBuildSettlement while playing and on water");
+		}
+	}
+	
+	@Test
+	public void testDoBuildSettlement7() {
+		VertexLocation vertLoc = new VertexLocation(new HexLocation(0,1), VertexDirection.East);
+		Model model = null;
+		try {
+			model = new Model(getJSONFrom("robbing.json"));
+			model.doBuildSettlement(false, vertLoc, 0);
+		} catch (BadJSONException e) {
+			fail("Failed doBuildSettlement while initilizing model");
+		} catch (ViolatedPreconditionException e) {
+			System.out.println("passed doBuildSettlement while robbing");
+		}
+		if(!model.equalsJSON(getJSONFrom("robbing.json"))) {
+			fail("Failed doBuildSettlement while in second robbing");
+		}
+	}
+	
+	@Test
+	public void testDoBuildSettlement8() {
+		VertexLocation vertLoc = new VertexLocation(new HexLocation(0,1), VertexDirection.East);
+		Model model = null;
+		try {
+			model = new Model(getJSONFrom("notYourTurn.json"));
+			model.doBuildSettlement(false, vertLoc, 0);
+		} catch (BadJSONException e) {
+			fail("Failed doBuildSettlement while initilizing model");
+		} catch (ViolatedPreconditionException e) {
+			System.out.println("passed doBuildSettlement while not your turn");
+		}
+		if(!model.equalsJSON(getJSONFrom("notYourTurn.json"))) {
+			fail("Failed doBuildSettlement while not your turn");
+		}
+	}
+	
+	@Test
+	public void testDoBuildSettlement9() {
+		VertexLocation vertLoc = new VertexLocation(new HexLocation(0,1), VertexDirection.East);
+		Model model = null;
+		try {
+			model = new Model(getJSONFrom("notEnoughResources.json"));
+			model.doBuildSettlement(false, vertLoc, 0);
+		} catch (BadJSONException e) {
+			fail("Failed doBuildSettlement while initilizing model");
+		} catch (ViolatedPreconditionException e) {
+			System.out.println("passed doBuildSettlement with not enough resources");
+		}
+		if(!model.equalsJSON(getJSONFrom("notEnoughResources.json"))) {
+			fail("Failed doBuildSettlement with not enough resources");
+		}
+	}
+	
+	@Test
+	public void testDoBuildSettlement10() {
+		VertexLocation vertLoc = new VertexLocation(new HexLocation(0,1), VertexDirection.East);
+		Model model = null;
+		try {
+			model = new Model(getJSONFrom("playing.json"));
+			model.doBuildSettlement(false, vertLoc, 0);
+		} catch (BadJSONException e) {
+			fail("Failed doBuildSettlement while initilizing model");
+		} catch (ViolatedPreconditionException e) {
+			fail("Failed doBuildSettlement while playing");
+		}
+		if(!model.equalsJSON(getJSONFrom("placedSettlement.json"))) {
+			System.out.println("passed doBuildSettlement while playing");
+		} else {
+			fail("Failed doBuildSettlement while playing");
+		}
+	}
+	
+	@Test
+	public void testDoBuildSettlement11() {
+		VertexLocation vertLoc = new VertexLocation(new HexLocation(0,0), VertexDirection.East);
+		Model model = null;
+		try {
+			model = new Model(getJSONFrom("playing.json"));
+			model.doBuildSettlement(false, vertLoc, 0);
+		} catch (BadJSONException e) {
+			fail("Failed doBuildSettlement while initilizing model");
+		} catch (ViolatedPreconditionException e) {
+			System.out.println("passed doBuildSettlement with taken location");
+		}
+		if(!model.equalsJSON(getJSONFrom("playing.json"))) {
+			fail("Failed doBuildSettlement with taken location");
+		}
+	}
+	
+	@Test
+	public void testDoBuildSettlement12() {
+		VertexLocation vertLoc = new VertexLocation(new HexLocation(0,1), VertexDirection.East);
+		Model model = null;
+		try {
+			model = new Model(getJSONFrom("playing.json"));
+			model.doBuildSettlement(false, vertLoc, 99);
+		} catch (BadJSONException e) {
+			fail("Failed doBuildSettlement while initilizing model");
+		} catch (ViolatedPreconditionException e) {
+			System.out.println("passed doBuildSettlement with invalid index");
+		}
+		if(!model.equalsJSON(getJSONFrom("playing.json"))) {
+			fail("Failed doBuildSettlement with invalid index");
+		}
+	}
+	
 }
--- java/test/test/Test.java
+++ java/test/test/Test.java
@@ -38,16 +38,16 @@ public class Test {
 				"model.can.build.city.CanBuildCityTest"*/
 				
 			// Phase Three tests
-				"model.execute.send.DoSendChatTest",
+//				"model.execute.send.DoSendChatTest",
 				///"model.execute.DoBuyDevCardTest",
 				
-				"model.execute.roll.DoRollNumberTest",
+//				"model.execute.roll.DoRollNumberTest",
 				/*"model.execute.placerobber.DoPlaceRobberTest",
 				"model.execute.finishturn.DoFinishTurnTest",*/
-				"model.execute.discardcards.DoDiscardCardsTest",
+//				"model.execute.discardcards.DoDiscardCardsTest",
 				
 				//"model.execute.trade.offer.DoOfferTradeTest",
-				"model.execute.trade.accept.DoAcceptTradeTest",
+//				"model.execute.trade.accept.DoAcceptTradeTest",
 				/*"model.execute.trade.maritime.DoMaritimeTradeTest",
 				
 				"model.execute.use.yearofplenty.DoUseYearOfPlentyTest",
@@ -56,8 +56,8 @@ public class Test {
 				"model.execute.use.monopoly.DoUseMonopolyTest",
 				"model.execute.use.monument.DoUseMonumentTest",*/
 				
-				"model.execute.build.road.DoBuildRoadTest",
-				//"model.execute.build.settlement.DoBuildSettlementTest",
+//				"model.execute.build.road.DoBuildRoadTest",
+				"model.execute.build.settlement.DoBuildSettlementTest",
 				//"model.execute.build.city.DoBuildCityTest"
 				
 		};
