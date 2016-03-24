5f619b79addff550ca7188b7bfa0dd3b85790efd
Joshua Haertel <josh.haertel@gmail.com>
2016-03-23 20:22:14 -0600

    join and list game

--- java/src/server/command/games/list.java
+++ java/src/server/command/games/list.java
@@ -1,16 +1,25 @@
 package server.command.games;
 
+import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 
+import server.data.ServerKernel;
 import server.exception.ServerAccessException;
 
 public class list extends GamesCommand {
 
 	@Override
-	public String execute(JSONObject args, String cookie) throws ServerAccessException {
-		return null;
-		// TODO Auto-generated method stub
-
+	public String execute(JSONObject args, String cookie) 
+			throws ServerAccessException {
+		String result = null;
+		if(this.validCookie(cookie)) {
+			JSONArray games = ServerKernel.sole().getGames();
+			result = games.toJSONString();
+		} else {
+			throw new ServerAccessException("Invalid Cookie");
+		}
+		
+		return result;
 	}
 
 }
