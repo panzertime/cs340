85c30a6bbd7809bf5e67a80fe2809128077d1eb1
JR Walther <jrwalther@gmail.com>
2016-04-07 20:48:23 -0600

    Messing around with AI - don't pull from here

--- java/src/server/command/game/addAI.java
+++ java/src/server/command/game/addAI.java
@@ -2,18 +2,33 @@ package server.command.game;
 
 import org.json.simple.JSONObject;
 
+import server.data.AI;
+import server.data.ServerKernel;
+import server.data.User;
 import server.exception.ServerAccessException;
+import server.utils.CatanCookie;
+import server.utils.CookieException;
 
 public class addAI extends GameCommand {
+
+	private CatanCookie cookie;
 
 	@Override
 	public String execute(JSONObject args, String cookie) 
 			throws ServerAccessException {
 		if(validCookie(cookie))
 		{
-			throw new ServerAccessException("Can't Add AI's");
+			User user = new AI();
+			ServerKernel.sole().addUser(user);
+			try {
+				this.cookie = new CatanCookie(user);
+			} catch (CookieException e) {
+			}	
+			;
+			//throw new ServerAccessException("Can't Add AI's");
 		} else {
 			throw new ServerAccessException("Invalid Cookie");
 		}
+		return "Sucess";
 	}
 }
