87788c1b0d560ca1b0f5aed87e214fc864e0b3fa
JR Walther <jrwalther@gmail.com>
2016-04-07 22:23:51 -0600

    SQL

--- java/src/server/persistance/IDAOFactory.java
+++ java/src/server/persistance/IDAOFactory.java
@@ -6,18 +6,18 @@ public interface IDAOFactory {
 	 * @return UsersDAO DAO to interact with the database implementation containing the users
 	 * @throws DatabaseException
 	 */
-	public IUsersDAO createUsersDAO() throws DatabaseException;
+	public IUsersDAO createUsersDAO(PersistanceManager pm) throws DatabaseException;
 	
 	/**
 	 * @return GamesDAO DAO to interact with the database implementation containing the games
 	 * @throws DatabaseException
 	 */
-	public IGamesDAO createGamesDAO() throws DatabaseException;
+	public IGamesDAO createGamesDAO(PersistanceManager pm) throws DatabaseException;
 	
 	/**
 	 * @return CommandsDAO DAO to interact with the database implementation containing the commands
 	 * @throws DatabaseException
 	 */
-	public ICommandsDAO createCommandsDAO() throws DatabaseException;
+	public ICommandsDAO createCommandsDAO(PersistanceManager pm) throws DatabaseException;
 	
 }
