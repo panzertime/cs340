e771cad353fb6b0fd89e39efb5863a4e26f7b9e5
JR Walther <jrwalther@gmail.com>
2016-04-11 21:56:05 -0600

    More functions

--- java/src/server/persistance/files/FileGamesDAO.java
+++ java/src/server/persistance/files/FileGamesDAO.java
@@ -1,14 +1,25 @@
 package server.persistance.files;
 
+import java.io.BufferedReader;
+import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FilenameFilter;
+import java.io.IOException;
 import java.io.PrintWriter;
 import java.io.UnsupportedEncodingException;
+import java.util.ArrayList;
 import java.util.List;
+
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
 
 import server.persistance.DatabaseException;
 import server.persistance.IConnection;
 import server.persistance.IGamesDAO;
 import shared.model.Model;
+import shared.model.exceptions.BadJSONException;
 
 public class FileGamesDAO implements IGamesDAO {
 
@@ -24,10 +35,38 @@ public class FileGamesDAO implements IGamesDAO {
 
 	}
 
+	
+	
 	@Override
 	public List<Model> getGames(IConnection connection) throws DatabaseException {
-		// TODO Auto-generated method stub
-		return null;
+		List<Model> result = new ArrayList<Model>();
+		File dir = new File("data/game/");
+
+    	File[] files = dir.listFiles(new FilenameFilter() { 
+    	         public boolean accept(File dir, String filename)
+    	              { return filename.endsWith(".txt"); }
+    	} );
+    	JSONParser jp = new JSONParser();
+    	
+    	for (File f: files) {
+    		String jsontext = null;
+    		BufferedReader in;
+    		try {
+    		    in = new BufferedReader(new FileReader(f));
+    		    String str;
+    		    while ((str = in.readLine()) != null)
+    		        jsontext.concat(str);
+    			in.close();
+    		} catch (IOException e) {
+    			throw new DatabaseException(e);
+    		} 
+    		try {
+				result.add(new Model((JSONObject) jp.parse(jsontext)));
+			} catch (BadJSONException | ParseException e) {
+				throw new DatabaseException(e);
+			}
+    	}		
+		return result;
 	}
 
 }
--- java/src/server/persistance/files/FileUsersDAO.java
+++ java/src/server/persistance/files/FileUsersDAO.java
@@ -1,14 +1,26 @@
 package server.persistance.files;
 
+import java.io.BufferedReader;
+import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FilenameFilter;
+import java.io.IOException;
 import java.io.PrintWriter;
 import java.io.UnsupportedEncodingException;
+import java.util.ArrayList;
 import java.util.List;
+
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
 
 import server.data.User;
 import server.persistance.DatabaseException;
 import server.persistance.IConnection;
 import server.persistance.IUsersDAO;
+import shared.model.Model;
+import shared.model.exceptions.BadJSONException;
 
 public class FileUsersDAO implements IUsersDAO {
 
@@ -27,8 +39,37 @@ public class FileUsersDAO implements IUsersDAO {
 
 	@Override
 	public List<User> getUsers(IConnection connection) throws DatabaseException {
-		// TODO Auto-generated method stub
-		return null;
+		List<User> result = new ArrayList<User>();
+		File dir = new File("data/user/");
+
+    	File[] files = dir.listFiles(new FilenameFilter() { 
+    	         public boolean accept(File dir, String filename)
+    	              { return filename.endsWith(".txt"); }
+    	} );
+    	JSONParser jp = new JSONParser();
+    	
+    	for (File f: files) {
+    		String jsontext = null;
+    		BufferedReader in;
+    		try {
+    		    in = new BufferedReader(new FileReader(f));
+    		    String str;
+    		    while ((str = in.readLine()) != null)
+    		        jsontext.concat(str);
+    			in.close();
+    		} catch (IOException e) {
+    			throw new DatabaseException(e);
+    		} 
+    		try {
+				result.add(new User((JSONObject) jp.parse(jsontext)));
+			} catch (BadJSONException | ParseException e) {
+				throw new DatabaseException(e);
+			}
+    	}
+		
+		
+		return result;
+
 	}
 
 }
